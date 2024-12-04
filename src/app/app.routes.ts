8586a32c4630c5a667be25a113a52f1aedba5e35
Adrihan249 <adrihanmendoza2018@gmail.com>
2024-12-03 21:12:07 -0500

    feat(ui): agregar vista de blog
    - Se agrego el componente para la vista  de contacto
    - Se agrego animaciones ala vista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SupportComponent } from './features/soporte-tecnico/support.component';
+import { BlogComponent } from './features/blog/blog.component';
+import { BlogC1Component } from './features/blog/bloC1.component';
+import { BlogC2Component } from './features/blog/bloC2.component';
+import { BlogC3Component } from './features/blog/bloC3.component';
 
 export const routes: Routes = [
   {
@@ -21,6 +25,22 @@ export const routes: Routes = [
       {
         path: 'soporte-tecnico',
         component: SupportComponent,
+      },
+      {
+        path: 'blog', // Ruta para el blog
+        component: BlogComponent,
+      },
+      {
+        path: 'blogC3', // Ruta para el blog
+        component: BlogC3Component,
+      },
+      {
+        path: 'blogC2', // Ruta para el blog
+        component: BlogC2Component,
+      },
+      {
+        path: 'blogC1',
+        component: BlogC1Component,
       } /*
       EJEMPLO DE COMO AGREGAR UNA NUEVA RUTA
       {
