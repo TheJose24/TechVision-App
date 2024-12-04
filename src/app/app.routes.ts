c18ab8da0ed371c770ad9198bd1bb3c369be9766
TheJose <devbyjose@gmail.com>
2024-12-04 06:56:16 -0500

    build(config): corrreccion package.json
    - Se corrigio los archivos package.json y package.lock.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -51,7 +51,7 @@ export const routes: Routes = [
       {
         path: 'contacto',
         component: ContactoComponent,
-      }
+      },
       /*
       EJEMPLO DE COMO AGREGAR UNA NUEVA RUTA
       {
